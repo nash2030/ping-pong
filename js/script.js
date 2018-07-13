62d101cf922866f02bd47e11680fa1300de56f05
Nathan Ongoya <nathanongoya@gmail.com>
2018-07-13 17:01:41 +0300

    add business logic

---
+++ js/script.js
@@ -0,0 +1,18 @@
+var numberInput = [];
+
+  function ping (number){
+    for (var index = 1; index <= number; index+=1){
+      if(index % 3 === 0){
+            numbersInput.push("ping");
+        }
+        else if(index % 5 === 0){
+            numbersInput.push("pong");
+          }
+        else if(index % 15 === 0){
+            numberInput.push("pingpong");
+        }
+        else{
+            numberInput.push(index);
+        }
+
+  }

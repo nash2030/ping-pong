6e826c0d790e41b723e6a4f336c77e67676bf647
Nathan Ongoya <nathanongoya@gmail.com>
2018-07-16 11:42:35 +0300

    Add javascript and modify the display of the images

--- js/script.js
+++ js/script.js
@@ -29,3 +29,17 @@ var numberInput = [];
       });
     });
   });
+var slideIndex = 0;
+carousel();
+
+carousel() {
+  var index;
+  var caption = document.getElementsByClassName("slideshow");
+  for (index = 0,index < caption.length, index++) {
+  caption[index].style.display = "none";
+}
+slideIndex++;
+if (slideIndex>caption.length) {slideIndex=1}
+caption[slideIndex-1].style.display = "block";
+setTimeout(carousel, 2000);
+}
